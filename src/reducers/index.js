7bd5ef5e554909dbf52993af13ca7a30bd41378e
lizparody <lizparody@gmail.com>
2017-10-09 16:30:18 -0500

    initiation of the project

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import BooksReducer from './reducer_books';
 
-const rootReducer = combineReducers({
-  state: (state = {}) => state
+const rootReducer = combineReducers({ //How to create the application state
+  books: BooksReducer
 });
 
 export default rootReducer;
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () { //Create the BooksReducer
+  return [
+    { title: 'A Song of Ice and Fire'}
+    { title: 'The Old And The Sea'}
+    { title: 'A Hundred Years of Solitud'}
+    { title: 'The Power of Your Subconscious Mind'}
+  ]
+}
