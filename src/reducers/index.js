1d9142c38b7646d09178f935d824aaa66665c040
lizparody <lizparody@gmail.com>
2017-10-10 12:12:54 -0500

    finished the book details view

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,9 @@ import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books';
 import ActiveBook from './reducer_active_book'
 
-const rootReducer = combineReducers({ //to create the application state
+const rootReducer = combineReducers({ //All the reducers get tied together in combineReducers. Every key passed on combineReducers end up as a key on our global state
   books: BooksReducer, //this reducer is going to add a key to our global app state called books, where the key is books and the value is whatever gets returned from the books reducer which in this case is our array of books
-  activeBook: ActiveBook //Every key passed on combineReducers end up as a key on our global state
+  activeBook: ActiveBook //whatever ActiveBook returns, will be available as activeBook.
 });
 
 export default rootReducer;
