bb9f8d9bd2d1af90669560de6c4db95e39e3db1f
lizparody <lizparody@gmail.com>
2017-11-01 20:50:53 -0500

    changed the style of the cursor

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -19,7 +19,7 @@ class BookList extends Component {
   }
   render() {
     return (
-      <ul className="list-group col-sm-4" style={{'padding-top': '50px'}}>
+      <ul className="list-group col-sm-4" style={{'padding-top': '50px', 'cursor': 'pointer'}}>
         {this.renderList()} { /* It calls the renderList helper function */}
       </ul>
     )
